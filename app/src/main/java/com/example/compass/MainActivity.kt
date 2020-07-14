cc5ac45622d575730e7852a8a05f55320f4ac08d
Szczepan <l.szczepan@wp.pl>
2020-07-14 14:41:21 +0200

    providing heading and bearing with fixed destination long / lat

--- app/src/main/java/com/example/compass/MainActivity.kt
+++ app/src/main/java/com/example/compass/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.example.compass
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.example.compass.databinding.ActivityMainBinding
-import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
+    private lateinit var locationManager: LocationManager
     private lateinit var binding: ActivityMainBinding
     private lateinit var accelerometer: Sensor
     private lateinit var magnetometer: Sensor
     private lateinit var gravityList: FloatArray
     private lateinit var magneticList: FloatArray
     private var heading = 0
+    private var longDest = 17.0
+    private var latDest = 52.0
+    private var currentLong = 0.0
+    private var currentLat = 0.0
+    private lateinit var currentLocation: Location
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,13 +39,34 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                1
+            )
+        }
+        currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        currentLong = currentLocation.longitude
+        currentLat = currentLocation.latitude
     }
 
     override fun onResume() {
         super.onResume()
 
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI)
-        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI)
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
@@ -61,14 +93,26 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             if (success) {
                 val orientation = FloatArray(3)
                 SensorManager.getOrientation(R, orientation)
-                heading = orientation[0].toDegrees()
+                heading = orientation[0].to360Degrees()
             }
         }
 
         binding.heading = heading
+        binding.azimuth = currentLocation.bearingTo(Location("").apply {
+            latitude = latDest
+            longitude = longDest
+        }).toPositiveDegrees()
     }
 }
 
-private fun Float.toDegrees(): Int {
+private fun Float.toPositiveDegrees(): Int {
+    return if (this < 0.0) {
+        this.plus(360).toInt()
+    } else {
+        this.toInt()
+    }
+}
+
+private fun Number.to360Degrees(): Int {
     return Math.toDegrees(this.toDouble()).plus(360).rem(360).toInt()
 }
