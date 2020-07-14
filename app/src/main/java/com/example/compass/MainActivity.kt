2f4877e8db0873aa1925e17f6c3dcdbb746f3574
Szczepan <l.szczepan@wp.pl>
2020-07-14 09:05:51 +0200

    displaying current heading with numbers, using only one sensor

--- app/src/main/java/com/example/compass/MainActivity.kt
+++ app/src/main/java/com/example/compass/MainActivity.kt
@@ -1,22 +1,48 @@
 package com.example.compass
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.compass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private val currentHeading = 0
+    private lateinit var sensorManager: SensorManager
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.heading = currentHeading
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        sensorManager.unregisterListener(this)
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("Not yet implemented")
+
     }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        TODO("Not yet implemented")
+    override fun onSensorChanged(event: SensorEvent) {
+        var degree = Math.round(event.values[0])
+
+        binding.heading = degree
     }
 }
